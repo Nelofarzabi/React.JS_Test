16b9866001c3559224f111acadb1492a759b0677
Nilofar <Nelofarzabi2000@gmail.com>
2021-08-02 06:28:49 -0700

    edit file and solve the problem

--- src/state.js
+++ src/state.js
@@ -1,10 +1,14 @@
 
 export const StateComp = () => {
-    <div>
+    return (
+        <div>
         <h1> Hello React  </h1>
         <h2>this is the default export !!!!! </h2>
 
     </div>
 
+
+    )
+   
 
 }
