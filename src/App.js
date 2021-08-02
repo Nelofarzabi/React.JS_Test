900a1550dd70170a76d7d1154e332c17da37b5a0
Nilofar <Nelofarzabi2000@gmail.com>
2021-08-02 06:08:53 -0700

    working with functional component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,39 @@
 
+import React from 'react';
 import './App.css';
 // import Loop from './state';
 // import POP from './props';
 // import Clasebase from './classbase';
 // import Pickernumber from './pickerNumber';
-import State from "./state";
+import StateComp from "./state";
 
-
-function App() {
+function App(){
   return (
     <div className="App">
-      <div>
         {/* <Clasebase/> */}
         {/* <Pickernumber/> */}
         {/* <POP
         number = {1}
         number1 = {1}
         number2 = {1}
-       
         />
          <POP
         number = {1}
         number1 = {3}
         number2 = {1}
-       
-        />
+       />
         <POP
         number = {2}
         number1 = {2}
         number2 = {2}
-       
-        /> */}
-        <State
-        About ='This is nelofar zabi she is student in kabul univercity she is intelligent girl'
-        />
-        
-
-
-       
-      
-       
-      </div>
+       /> */}
+       <StateComp />
+     
     </div>
   );
+
 }
+
 
 export default App;
 
--- src/state.js
+++ src/state.js
@@ -1,29 +1,6 @@
-import React  from "react";
+// import React from 'react ';
 
-class  State extends  React.Component{
-    //State section
-
-
-    constructor(){
-        super();
-        this.State={
-            name : 'Nelofar',
-            fatherName :'Shir ali zabi', 
-            id : 12 
-
-        }
-
-    }
-    render (){
-        return(
-            <div>
-                <h1>Hello  {this.State.name}</h1>
-                <p>{this.props.About}</p>
-            </div>
-        );
-
-    }
-
+function StateComp (){
+    return <h1>Hello Nelofar </h1>
 }
-export  default State ;
- 
+export default StateComp;
